5167511470daba61dc249fbf290e4b95e4245327
Renan Rocha <renansouza2310@gmail.com>
2022-02-06 11:10:01 -0300

    Testando a primeira classe model

--- src/main/java/com/example/cursomc/controller/CategoriaController.java
+++ src/main/java/com/example/cursomc/controller/CategoriaController.java
@@ -1,15 +1,26 @@
 package com.example.cursomc.controller;
 
+import com.example.cursomc.model.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "Rest está funcionando.";
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria("Informática");
+        Categoria cat2 = new Categoria("Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
---
+++ src/main/java/com/example/cursomc/model/Categoria.java
@@ -0,0 +1,55 @@
+package com.example.cursomc.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Categoria implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public Categoria() {
+
+    }
+
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
